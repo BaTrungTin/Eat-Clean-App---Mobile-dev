e39d83ca5490e7f537d530f06c3e6b75f8b8abfb
NguyenHongDong <ndong5373@gmail.com>
2025-10-23 00:18:34 +0700

    code UI Splash figma

--- app/src/main/java/com/team/eatcleanapp/MainActivity.kt
+++ app/src/main/java/com/team/eatcleanapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.team.eatcleanapp.ui.screens.splash.SplashScreen
 import com.team.eatcleanapp.ui.theme.EatCleanAppMobiledevTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EatCleanAppMobiledevTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                SplashScreen(){
                 }
             }
         }
