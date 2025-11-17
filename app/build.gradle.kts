69ed9a2f94e9b60eecacc741e97a0a37adaf4d7e
NguyenHongDong <ndong5373@gmail.com>
2025-11-17 20:24:55 +0700

    Fix AppDatabase and Gradle on main after merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,14 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-<<<<<<< HEAD
+
     alias(libs.plugins.kotlin.kapt)
-=======
-    id("org.jetbrains.kotlin.kapt")
+
+
 // hilt
     id("com.google.dagger.hilt.android")
 
->>>>>>> feature/health-dong
+
 }
 
 android {
@@ -85,23 +85,13 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
     implementation("androidx.compose.material:material-icons-extended")
 
-<<<<<<< HEAD
-    // Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-=======
+
+
     // ===== Hilt =====
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-compiler:2.52")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
 
->>>>>>> feature/health-dong
+
 }
